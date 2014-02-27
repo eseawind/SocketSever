741db5f7a3f1fe1c9f943a53af6dca06a8f739e5
lincolnge <326684793@qq.com>
2014-02-27 08:52:23 +0800

    Create a framework

---
+++ src/MainServer.java
@@ -0,0 +1,52 @@
+import java.awt.AWTException;
+import java.awt.HeadlessException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.imageio.ImageIO;
+
+public class MainServer {
+  private static final int PORT = 9999;  
+  public static void main(String[] args) throws HeadlessException, AWTException, IOException {
+    
+    String ip = null;
+    ip = InetAddress.getLocalHost().getHostAddress();
+    System.out.println(ip);
+
+    for(;;){
+      BufferedImage screencapture = new Robot().createScreenCapture(
+          new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()) );
+      
+      ByteArrayOutputStream out = new ByteArrayOutputStream();
+      ImageIO.write(screencapture, "jpg", out);
+      byte[] b = out.toByteArray();
+      
+      // save in local
+//      File file = new File("screencapture.jpg");
+//      ImageIO.write(screencapture, "jpg", file);
+
+      try {
+        ServerSocket server = new ServerSocket(PORT);
+        Socket socket = server.accept();
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+
+        dos.writeInt(b.length);
+        dos.write(b);
+
+        dos.flush();
+        dos.close();
+        socket.close();
+        server.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
